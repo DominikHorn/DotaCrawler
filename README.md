1f3f855e027c9aca24ae043d7b128cc37c4f5296
Dominik Horn <dominik.horn@tum.de>
2021-02-27 10:53:03 +0100

    add setup script for initial repository setup

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Setup
+# Development Setup
 
 Before this crawler can begin harvesting data a steam developer 
 token is required. You may request one [here](http://steamcommunity.com/dev/apikey).
+
+Run the `setup.sh` script to setup this repository. It will automatically
+create, e.g., the required .env file.
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROJECT_DIR=$(dirname "$0")
+
+TEMPLATE_FILE=template.env
+OUTPUT_FILE=.env
+
+if [[ -f "${OUTPUT_FILE}" ]]; then
+  echo "File \"${OUTPUT_FILE}\" exists already. Please remove it manually before running this script again."
+  exit -1
+fi
+
+while read -u 10 KEY || [[ -n $KEY ]]; do
+  read -p "${KEY}" VALUE
+  echo "${KEY}${VALUE}" >> ${OUTPUT_FILE}
+done 10<"${PROJECT_DIR}/${TEMPLATE_FILE}"
